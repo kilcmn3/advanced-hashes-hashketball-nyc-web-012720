24a737863d5a8e521b1f160873fa6c7f23e781d8
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-17 09:16:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,5 +130,5 @@ def big_shoe_rebounds
   result_home = Hash[home_player.collect {|x| [x[:collect] > x[:collect], x[:player_name]]}]
   result_away = Hash[away_player.collect {|x| [x[:shoe], x[:player_name]]}]
   pp result_home
-  end
+  
 end
