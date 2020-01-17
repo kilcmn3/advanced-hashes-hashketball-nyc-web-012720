baa97c8bf33683e764110e63d3f75067bcfe8ddf
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-17 09:34:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,7 +128,7 @@ def big_shoe_rebounds
   away_player = game_hash[:away][:players]
   final_answer = nil
 
-  result_home = Hash[home_player.collect {|x| x[:player_name], [x[:shoe]]}]
-  result_away = Hash[away_player.collect {|x| x[:player_name], [x[:shoe]]}]
+  result_home = Hash[home_player.collect {|x| [x[:player_name], x[:shoe]]}]
+  result_away = Hash[away_player.collect {|x| [x[:player_name], x[:shoe]]}]
 puts result_home.max{|key, value| value}
 end
