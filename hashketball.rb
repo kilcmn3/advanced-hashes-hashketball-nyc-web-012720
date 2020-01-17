559680e0b39149b1155bb2c925e1fad7ea97770c
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-17 08:23:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,6 +92,6 @@ def player_numbers(name)
   away_player = game_hash[:away]
 
     if home_player[:team_name] == name
-        home_player[:players].find_all {|x| pp x[:number]  }
+        home_player[:players].find_all {|x|  x[:number]  }
     end
 end
