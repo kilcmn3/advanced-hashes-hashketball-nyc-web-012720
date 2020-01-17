194a340081cdc188a2755e3f8905b8fefed03b7d
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-17 08:55:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,6 +113,7 @@ def player_stats(name)
     while i < home_player.length do
       if home_player[i][:player_name] == name
           result["#{name}"] = home_player[i]
+          result = result["#{name}"].delete(:player_name)
       elsif away_player[i][:player_name] == name
           result["#{name}"] = away_player[i]
         end
